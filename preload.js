fc80355c75e4c48bad62b8c2b8e9beb0e44c4dbe
lioncat2002 <sankhayan2002@gmail.com>
2022-04-16 14:30:16 +0530

    feat: api detecting voice

--- preload.js
+++ preload.js
@@ -46,42 +46,109 @@ window.addEventListener("DOMContentLoaded", async () => {
     ipc.send("terminal.keystroke", e);
   });
 
-  const chunks = [];
   function errorCallback(e) {
     console.log("Error", e);
   }
 
-  let mediaRecorder;
-
   const btn = document.getElementById("recordBtn");
-
-  const audioChunks = [];
 
   function onRecordClicked(e) {
     if (btn.innerHTML === "Record!") {
-      btn.innerHTML = "Stop!";
-      navigator.mediaDevices
-        .getUserMedia({ video: false, audio: true })
-        .then((localMediaStream) => {
-          mediaRecorder = new MediaRecorder(localMediaStream);
-          mediaRecorder.start();
+      btn.innerHTML="Recording!";
+      const accessToken =  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjUxMDQwODUzMzMxODA0MTYiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiVXJEc1daRHVFdlg5d1E4RDU2dEV4V0xvN0RlTUVYWjRAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjUwMDk0MTU4LCJleHAiOjE2NTAxODA1NTgsImF6cCI6IlVyRHNXWkR1RXZYOXdROEQ1NnRFeFdMbzdEZU1FWFo0IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.LCgl7DuCH5BsgkR8EQfTMrpEIJW0s7EFSlwhkvdCl7wP6boJR9YYRABjSa6dhhAwOBNWVSjK3DMqs0PCmiai0_I91gVfcr5dGQL8X15v5P2iJmThmFr6gWWVkQud338VvWtjV7VRnidH_oCcDSQlmZM04iFJMKeFkc0IfSN0VukReBaof5QkZqsxVMWklZUG_ZdBRWPe4G_tudYo2xHic6VT5glHN2a8fymg9DMUiQLYDWoUAlYnrwGyJBO1-aZPCoNWmrCeSfGkNsx8rrJYETKArQQi_gy2mgEaWpkzlXsJ_xEFowje9d_fCdDOxY2-vVuyy7fuHNfgUY3d7slH9Q';
+// Refer to the section for how to generate the accessToken: https://docs.symbl.ai/docs/developer-tools/authentication
+const uniqueMeetingId = btoa("gamedevcorp2002@gmail.com"); // btoa will take a string and generate a unique ID
+const symblEndpoint = `wss://api.symbl.ai/v1/streaming/${uniqueMeetingId}?access_token=${accessToken}`;
 
-          mediaRecorder.addEventListener("dataavailable", (event) => {
-            audioChunks.push(event.data);
-          });
+const ws = new WebSocket(symblEndpoint);
 
-          mediaRecorder.addEventListener("stop", () => {
-            const audioBlob = new Blob(audioChunks);
-            const audioUrl = URL.createObjectURL(audioBlob);
+// Fired when a message is received from the WebSocket server
+ws.onmessage = (event) => {
+  // You can find the conversationId in event.message.data.conversationId;
+  const data = JSON.parse(event.data);
+  if (data.type === 'message' && data.message.hasOwnProperty('data')) {
+    console.log('conversationId', data.message.data.conversationId);
+  }
+  if (data.type === 'tracker_response') {
+    for (let tracker of data.trackers) {
+      for (let match of tracker.matches) {
+        console.log('Tracker found:', match.value);
+      }
+    }
+  }
+  console.log(`Response type: ${data.type}. Object: `, data);
+};
 
-            const { data } = axios.post(
-              "https://api.symbl.ai/v1/process/audio"
-            );
-          });
-        });
-    } else {
+// Fired when the WebSocket closes unexpectedly due to an error or lost connetion
+ws.onerror  = (err) => {
+  console.error(err);
+};
+
+// Fired when the WebSocket connection has been closed
+ws.onclose = (event) => {
+  console.info('Connection to websocket closed');
+};
+
+// Fired when the connection succeeds.
+ws.onopen = (event) => {
+  ws.send(JSON.stringify({
+    type: 'start_request',
+    meetingTitle: 'Websockets How-to', // Conversation name
+    insightTypes: ['question', 'action_item'], // Will enable insight generation
+    trackers: [
+      {
+        name: "create",
+        vocabulary: [
+          "create",
+          "make a new"
+        ]
+      }
+    ],
+    config: {
+      confidenceThreshold: 0.5,
+      languageCode: 'en-US',
+      speechRecognition: {
+        encoding: 'LINEAR16',
+        sampleRateHertz: 44100,
+      }
+    },
+    speaker: {
+      userId: 'example@symbl.ai',
+      name: 'Example Sample',
+    }
+  }));
+};
+
+const handleSuccess = (stream) => {
+   
+  const AudioContext = window.AudioContext;
+  const context = new AudioContext();
+  const source = context.createMediaStreamSource(stream);
+  const processor = context.createScriptProcessor(1024, 1, 1);
+  const gainNode = context.createGain();
+  source.connect(gainNode);
+  gainNode.connect(processor);
+  processor.connect(context.destination);
+  processor.onaudioprocess = (e) => {
+    // convert to 16-bit payload
+    const inputData = e.inputBuffer.getChannelData(0) || new Float32Array(this.bufferSize);
+    const targetBuffer = new Int16Array(inputData.length);
+    for (let index = inputData.length; index > 0; index--) {
+        targetBuffer[index] = 32767 * Math.min(1, inputData[index]);
+    }
+    // Send audio stream to websocket.
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(targetBuffer.buffer);
+    }
+  };
+};
+
+
+stream = navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
+    } 
+    else {
       btn.innerHTML = "Record!";
-      mediaRecorder.stop();
+      //mediaRecorder.stop();
     }
   }
 
